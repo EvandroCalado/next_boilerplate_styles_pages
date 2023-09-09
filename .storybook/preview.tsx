dbc83d0132a159734ce6ba23a29584f5cdeacfd6
Evandro Calado <110628201+EvandroCalado@users.noreply.github.com>
2023-09-08 22:46:18 -0300

    :fix: add theme dark and light0

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,27 +1,39 @@
-import type { Preview } from "@storybook/react";
-import React from "react";
-import { GlobalStyles } from "../src/styles/global-styles";
-import Providers from "../src/utils/Providers";
+import type { Preview } from '@storybook/react';
+import React from 'react';
+import { GlobalStyles } from '../src/styles/global-styles';
+import { theme } from '../src/styles/theme';
+import Providers from '../src/utils/Providers';
 
 export const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
     },
+    backgrounds: {
+      default: 'light',
+      values: [
+        {
+          name: 'light',
+          value: theme.colors.backgroundLight,
+        },
+        {
+          name: 'dark',
+          value: theme.colors.backgroundDark,
+        },
+      ],
+    },
   },
 };
 
 export const decorators = [
   (Story) => (
-    <div>
-      <Providers>
-        <GlobalStyles />
-        <Story />
-      </Providers>
-    </div>
+    <Providers>
+      <GlobalStyles />
+      <Story />
+    </Providers>
   ),
 ];
